4e15f921d06846b31064bc18003087d9c60b2bf0
rafed01 <rafedriahi.rr@gmail.com>
2021-11-09 13:35:27 +0100

    header file

--- main.h
+++ main.h
@@ -1,4 +1,28 @@
 #ifndef MAINH
 #define MAINH
 
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct printf - struct for printf
+ * @c: The character
+ * @fun: The function associated
+ */
+typedef struct printf
+{
+	char c;
+	int (*fun)(va_list);
+} arr_of_p;
+
+int p_char(va_list arg);
+int p_string(va_list arg);
+int p_percent(va_list arg);
+int p_int(va_list arg);
+int _putchar(char c);
+int pick_function(const char s, va_list ap);
+
 #endif
